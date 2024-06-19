22ff18d8de41316b969f2a62b2731511580e946e
Gustavo de Luna Pinto <gustavo.pinto@arcelormittal.com.br>
2024-06-19 17:38:30 -0300

    feat: added github actions and data pipeline as python file

---
+++ pipe_data_processing.py
@@ -0,0 +1,31 @@
+from src.pipelines import pipeline_data_collection, pipeline_data_processing
+from datetime import datetime
+import time
+
+print(f"# Starting data pipeline at {datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}\n")
+
+id_deputados_to_fetch = [220526, 178873, 220527, 220530, 160517, 
+                         220528, 204356, 204355, 178871, 220529]
+start_date = '2024-06-01'
+end_date = '2024-06-01'
+
+
+update_tables = dict(despesas=True, deputados=False)
+
+t0 = time.time()
+pipeline_data_collection(update_tables, id_deputados_to_fetch, start_date, end_date)
+t1 = time.time()
+
+print(f"# Finished data collection in {round(t1 - t0, 1)} seconds\n")
+
+
+update_tables = dict(despesas=True, deputados=False)
+
+t0 = time.time()
+pipeline_data_processing(update_tables)
+t1 = time.time()
+
+print(f"# Finished data processing in {round(t1 - t0, 1)} seconds\n")
+
+
+print("# Finished data pipeline.")
