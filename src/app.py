4ba6be9ee99b5b756fbbdba491a2df376f0b11d0
Gustavo de Luna Pinto <gustavo.pinto@arcelormittal.com.br>
2024-07-11 16:57:05 -0300

    feat: moved some processing from app.py to app_visual.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,8 @@ st.set_page_config(layout="wide")
 import os
 import pandas as pd
 import plotly.express as px
-from app_visual import calc_avg_spend_from_period, biggest_spenders_per_type
+from app_visual import (calc_avg_spend_from_period, biggest_spenders_per_type,
+                        table_info_deputado)
 
 # RETRIEVE DATA
 df = pd.read_parquet(os.path.join('data','gold','master_table.parquet'))
@@ -42,18 +43,13 @@ df_filt = df.copy()
 df_mes_filt = df_mes.copy()
 if filter_type_spend:
     df_filt = df_filt.loc[df_filt['tipoDespesa']==filter_type_spend].copy()
-if filter_deputado:
-    df_filt = df_filt.loc[df_filt['nome']==filter_deputado].copy()
-    df_mes_filt = df_mes_filt.loc[df_mes_filt['Deputado']==filter_deputado].copy()
 if period:
     df_filt = df_filt.loc[df_filt['ano_mes'].isin(period)].copy()
     df_mes_filt = df_mes_filt.loc[df_mes_filt['ano_mes'].isin(period)].copy()
-
-
-biggest_spenders = biggest_spenders_per_type(df_filt)
-biggest_spenders.index = biggest_spenders.index.map(tipo_despesa_simplificado)
 if filter_deputado:
-    df_filt_dep = df_filt.loc[df_filt['nome']==filter_deputado].copy()
+    df_filt = df_filt.loc[df_filt['nome']==filter_deputado].copy()
+    df_mes_filt = df_mes_filt.loc[df_mes_filt['Deputado']==filter_deputado].copy()
+    df_filt_dep=df_filt.copy()  #deputado filtrado OU de maior gasto
 else:
     deputado_maior_gasto = df_mes_filt.groupby('Deputado')['Valor'].sum().idxmax()
     df_filt_dep = df_filt.loc[df_filt['nome']==deputado_maior_gasto].copy()
@@ -65,24 +61,23 @@ st.write('**Análise de cota parlamentar dos Deputados Federais**')
 
 # DISPLAY 1st row
 with st.container():
-    ### LEFT SIDE ###
     page_col = st.columns([0.65,0.35])
+    
+    ### LEFT SIDE ###
     page_col[0].metric(label="**Gasto médio mensal por deputado**", value=calc_avg_spend_from_period(df_mes_filt))
+
     fig = px.bar(df_filt, x='nome', y='Valor', color=df_filt['tipoDespesa'].map(tipo_despesa_simplificado), hover_data=['tipoDespesa'],
                 title='Despesas por deputado e tipo')
     fig.update_layout(legend=dict(title_text='Tipo de despesa'))
     page_col[0].plotly_chart(fig)
-    
+
+
     ### RIGHT SIDE ###
     # INFO FOR SPECIFIC 'DEPUTADO'
-    df_filt_dep['Categoria'] = df_filt_dep['tipoDespesa'].map(tipo_despesa_simplificado)
-    dep_filt_gastos_por_tipo = df_filt_dep.groupby('Categoria')[['Valor']].sum().round()
-    dep_filt_gastos_por_tipo['%'] = round(100*(dep_filt_gastos_por_tipo['Valor'] / dep_filt_gastos_por_tipo['Valor'].sum()),1)
-    dep_filt_gastos_por_tipo.sort_values(by='Valor',ascending=False, inplace=True)
-    foto_dep = df_filt_dep['urlFoto'].unique()[0]
+    info_dep, foto_dep = table_info_deputado(df_filt_dep, tipo_despesa_simplificado)
     page_col[1].write(f"**{df_filt_dep['nome'].unique()[0]}**")
     page_col[1].image(foto_dep, width=100, caption=f"Total gasto: R${round(df_filt_dep['Valor'].sum())}")
-    page_col[1].dataframe(dep_filt_gastos_por_tipo)
+    page_col[1].dataframe(info_dep)
 
 
 # DISPLAY 2nd row
@@ -91,6 +86,10 @@ with st.container():
     fig = px.line(df_mes, x='ano_mes', y='Valor', color='Deputado', markers=True, 
                   title = 'Histórico de Gastos Mensais por Deputado')
     page_col[0].plotly_chart(fig)
+
+    
+    biggest_spenders = biggest_spenders_per_type(df_filt)
+    biggest_spenders.index = biggest_spenders.index.map(tipo_despesa_simplificado)
     page_col[1].write("**Maior Gasto por Categoria no período**")
     page_col[1].dataframe(biggest_spenders)
 
